eda62841ad8906a812c1754e32a37dbebf109710
AdamGalla <ado.galla@gmail.com>
2023-10-09 13:23:33 +0200

    inject crypto helper

--- Password Manager Desktop Client/crypto/VaultCrypto.cs
+++ Password Manager Desktop Client/crypto/VaultCrypto.cs
@@ -3,19 +3,13 @@ using Web_Client.DTOs;
 
 namespace Password_Manager_Desktop_Client.crypto;
 
-public class VaultCrypto : IVaultCrypto
+internal class VaultCrypto : IVaultCrypto
 {
     private string _username;
     private string _password;
     private PasswordVaultDto _encryptedVault;
     private PasswordVaultDto _decryptedVault;
     private VaultKeyGenerator _vaultKeyGenerator;
-
-    public VaultCrypto(string username, string password)
-    {
-        _username = username;
-        _password = password;
-    }
 
     public PasswordVaultDto EncryptVault(PasswordVaultDto vault)
     {
