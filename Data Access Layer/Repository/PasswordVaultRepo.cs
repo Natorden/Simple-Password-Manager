0e91229dbaf21b35c7a67013b0114a493a8945cc
Natorden <71587437+Natorden@users.noreply.github.com>
2023-10-09 12:29:16 +0200

    Get Vault method implemented

--- Data Access Layer/Repository/PasswordVaultRepo.cs
+++ Data Access Layer/Repository/PasswordVaultRepo.cs
@@ -1,4 +1,7 @@
-﻿using Data_Access_Layer.Interfaces;
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Data_Access_Layer.Interfaces;
 using Data_Access_Layer.Models;
 
 namespace Data_Access_Layer.Repository;
@@ -6,15 +9,38 @@ namespace Data_Access_Layer.Repository;
 internal class PasswordVaultRepo : IPasswordVaultRepo
 {
     private readonly string _connectionString;
-
+    
     public PasswordVaultRepo(string connectionString)
     {
         _connectionString = connectionString;
     }
 
-    public async Task<PasswordVault> GetAsync(Guid ownerGuid)
+    
+    
+    public async Task<PasswordVault?> GetAsync(Guid ownerGuid)
     {
-        throw new NotImplementedException();
+        using var connection = new SqlConnection(_connectionString);
+        
+        DynamicParameters parameters = new DynamicParameters();   
+        parameters.Add("UserGuID", ownerGuid); 
+        
+        var vaults = await connection.QueryAsync<PasswordVault, IEnumerable<HashedCredentials>, PasswordVault>(
+            "GET_VAULT",
+            (vault, credentials) =>
+            {
+                if (vault.EncryptedVault == null)
+                    vault.EncryptedVault = new List<HashedCredentials>();
+
+                _ = vault.EncryptedVault.Concat(credentials);
+                return vault;
+            },
+            new { ownerGuid },
+            splitOn: "Sitename" // Specify the column to split the results on
+            , commandType: CommandType.StoredProcedure);
+        
+        // var hashedCreds = await connection.QuerySingleOrDefaultAsync<IEnumerable<HashedCredentials>>("GET_VAULT", parameters, commandType: CommandType.StoredProcedure);
+
+        return vaults.Distinct().FirstOrDefault();
     }
 
     public async Task<bool> UpdateAsync(Guid ownerGuid, PasswordVault vault)
