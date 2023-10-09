2650280065bcd2b91b54609689f547bdefd9d000
AdamGalla <ado.galla@gmail.com>
2023-10-09 13:26:49 +0200

    code cleanup

--- Password Manager Desktop Client/crypto/VaultCrypto.cs
+++ Password Manager Desktop Client/crypto/VaultCrypto.cs
@@ -5,11 +5,6 @@ namespace Password_Manager_Desktop_Client.crypto;
 
 internal class VaultCrypto : IVaultCrypto
 {
-    private string _username;
-    private string _password;
-    private PasswordVaultDto _encryptedVault;
-    private PasswordVaultDto _decryptedVault;
-    private VaultKeyGenerator _vaultKeyGenerator;
 
     public PasswordVaultDto EncryptVault(PasswordVaultDto vault, string username, string password)
     {
